5bee15c56d07947a18012715475c453460beeaf0
João Victor <joaovictor.code3@gmail.com>
2023-08-26 23:11:26 -0300

    Repositorio criado

---
+++ src/ContaTerminal.java
@@ -0,0 +1,12 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println();
+
+    }
+}
