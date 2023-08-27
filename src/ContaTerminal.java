7c9791839fcd126707cf67a35f927e3b30a64d28
João Victor <joaovictor.code3@gmail.com>
2023-08-27 16:11:43 -0300

    Desafio: ContaTerminal - finalizado!

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -6,7 +6,21 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println();
+        System.out.println("Número da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Nome do cliente: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é " + agencia + ", conta " + conta + " e seu saldo de R$" +
+                saldo + " já está disponível para saque.");
 
     }
 }
